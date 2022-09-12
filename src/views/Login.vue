e34ce006740daf6ae84f765173b47d24ba61cf0b
PauloScotti <paulo.scotti@hotmail.com>
2022-09-12 20:35:18 -0300

    aula 03 - iniciando o login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,47 @@
 <script lang="ts">
     import { defineComponent } from 'vue';
     export default defineComponent({
+        data(){
+            return {
+                login: '',
+                senha: '',
+                loading: false,
+                erro : ''
+            }
+        },
+        methods: {
+            efetuarLogin(){
+                if(!this.login && !this.senha){
+                    this.erro = "Favor preencher o formulários";
+                    return;
+                }
 
+                alert('Login efetuadi: ' + this.login + ', ' + this.senha)
+            }
+        }
     });
 </script>
 
 <template>
-    <h1>Bem vindo Devagram</h1>
+    <div>
+        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" />
+        <form action="">
+            <p v-if="erro">{{erro}}</p>
+            <div>
+                <img src="../assets/imagens/envelope.svg" alt="Insira o login" />
+                <input type="text" v-model="login">
+            </div>
+            <div>
+                <img src="../assets/imagens/chave.svg" alt="Insira a senha" />
+                <input type="password" v-model="senha">
+            </div>
+            <button @click.enter.prevent="efetuarLogin">
+                Login
+            </button>
+            <div>
+                <p>Não possui uma conta?</p>
+                <a href="">Faça seu cadastro agora!</a>
+            </div>
+        </form>
+    </div>
 </template>
