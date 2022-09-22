d03b5c7e4b478ccd09255ad457c6da00b59aa2f2
PauloScotti <paulo.scotti@hotmail.com>
2022-09-21 21:48:27 -0300

    resultado da busca

---
+++ src/services/UsuarioServices.ts
@@ -0,0 +1,7 @@
+import { HttpApiServices } from "./HttpApiServices"
+
+export class UsuarioServices extends HttpApiServices{
+    async pesquisar(filtro : String)    {
+        return this.get('pesquisa?filtro=' + filtro);
+    }
+}
