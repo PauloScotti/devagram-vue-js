b54a1468cebd289d2000d083b86b6f85a288d6d0
PauloScotti <paulo.scotti@hotmail.com>
2022-09-27 19:54:12 -0300

    aula 07 parte 05

--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -5,10 +5,14 @@ export class UsuarioServices extends HttpApiServices{
         return this.get('pesquisa?filtro=' + filtro);
     }
 
-    async buscarDadosPorId(id : String) {
+    async buscarDadosPorId(id? : String) {
         if(id){
             return await this.get('pesquisa?id=' + id);
         }
         return this.get('usuario');
+    }
+
+    async toggleFollow(id : String) {
+        return await this.put('seguir?id=' + id);
     }
 }
--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,39 +1,53 @@
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    import Header from '../components/Header.vue';
-    import Footer from '../components/Footer.vue';
-    import { FeedServices } from '../services/FeedServices';
-    import Feed from '../components/Feed.vue';
+import { defineComponent } from 'vue';
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+import { FeedServices } from '../services/FeedServices';
+import Feed from '../components/Feed.vue';
+import HeaderPerfil from '../components/HeaderPerfil.vue';
+import { UsuarioServices } from '@/services/UsuarioServices';
 
-    const feedServices = new FeedServices();
+const usuarioServices = new UsuarioServices();
 
-    export default defineComponent({
-        components: { Header, Footer, Feed },
-        data() {
-            return {
-                posts: []
-            }
-        },
-        async mounted(){
-            try{
-                const loggedId = localStorage.getItem('_id');
-                const id = loggedId as String;
+const feedServices = new FeedServices();
 
-                const resultado = await feedServices.getFeedPorId(id);
-                if(resultado && resultado.data){
-                    this.posts = resultado.data;
-                }
-
-            } catch(e){
-                console.log(e);
-            }
+export default defineComponent({
+    components: { Header, Footer, Feed, HeaderPerfil },
+    data() {
+        return {
+            posts: [],
+            usuario: {} as any
         }
-    });
+    },
+    async mounted() {
+        try {
+            const loggedId = localStorage.getItem('_id');
+            const id = loggedId as String;
+            const resultUsuario = await usuarioServices.buscarDadosPorId();
+            if(!resultUsuario || !resultUsuario.data){
+                return;
+            }
+            this.usuario = resultUsuario.data;
+            const result = await feedServices.getFeedPorId(id);
+            if (result && result.data) {
+                const postsFinal = result.data.map((p : any) => {
+                    p.usuario = resultUsuario.data;
+                    return p;
+                })
+                this.posts = postsFinal;
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
 </script>
 
 
 <template>
-    <Header />
-    <Feed :posts="posts" />
+    <Header :hide="true" />
+    <HeaderPerfil :usuario="usuario" :showLeft="false" :showRight="true" :isRightIcon="true" :title="usuario?.nome" v-if="usuario?._id"/>
+    <Feed :posts="posts" :temCabecalho="true" v-if="posts && posts.length > 0"/>
     <Footer />
 </template>
